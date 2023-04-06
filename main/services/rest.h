b6b6bd8ee9c845a13e6a9757434eff01f8aac6ea
David B. Adrian <dawidh.adrian@gmail.com>
2023-04-06 16:10:24 +0200

    add basic rest server from esp example

---
+++ main/services/rest.h
@@ -0,0 +1,219 @@
+#pragma once
+
+/* HTTP Restful API Server
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+#include "cJSON.h"
+
+#include "esp_chip_info.h"
+#include "esp_http_server.h"
+#include "esp_log.h"
+#include "esp_random.h"
+#include "esp_spiffs.h"
+#include "esp_vfs.h"
+
+#include <fcntl.h>
+#include <string.h>
+#include <algorithm>
+
+namespace hakkou {
+static const char* REST_TAG = "esp-rest";
+
+#define FILE_PATH_MAX (ESP_VFS_PATH_MAX + 128)
+#define SCRATCH_BUFSIZE (10240)
+
+typedef struct rest_server_context {
+  char base_path[ESP_VFS_PATH_MAX + 1];
+  char scratch[SCRATCH_BUFSIZE];
+} rest_server_context_t;
+
+#define CHECK_FILE_EXTENSION(filename, ext) \
+  (strcasecmp(&filename[strlen(filename) - strlen(ext)], ext) == 0)
+
+/* Set HTTP response content type according to file extension */
+static esp_err_t set_content_type_from_file(httpd_req_t* req,
+                                            const char* filepath) {
+  const char* type = "text/plain";
+  if (CHECK_FILE_EXTENSION(filepath, ".html")) {
+    type = "text/html";
+  } else if (CHECK_FILE_EXTENSION(filepath, ".js")) {
+    type = "application/javascript";
+  } else if (CHECK_FILE_EXTENSION(filepath, ".css")) {
+    type = "text/css";
+  } else if (CHECK_FILE_EXTENSION(filepath, ".png")) {
+    type = "image/png";
+  } else if (CHECK_FILE_EXTENSION(filepath, ".ico")) {
+    type = "image/x-icon";
+  } else if (CHECK_FILE_EXTENSION(filepath, ".svg")) {
+    type = "text/xml";
+  }
+  return httpd_resp_set_type(req, type);
+}
+
+/* Send HTTP response with the contents of the requested file */
+static esp_err_t rest_common_get_handler(httpd_req_t* req) {
+  char filepath[FILE_PATH_MAX];
+
+  rest_server_context_t* rest_context = (rest_server_context_t*)req->user_ctx;
+  strlcpy(filepath, rest_context->base_path, sizeof(filepath));
+  if (req->uri[strlen(req->uri) - 1] == '/') {
+    strlcat(filepath, "/index.html", sizeof(filepath));
+  } else {
+    strlcat(filepath, req->uri, sizeof(filepath));
+  }
+  int fd = open(filepath, O_RDONLY, 0);
+  if (fd == -1) {
+    ESP_LOGE(REST_TAG, "Failed to open file : %s", filepath);
+    /* Respond with 500 Internal Server Error */
+    httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR,
+                        "Failed to read existing file");
+    return ESP_FAIL;
+  }
+
+  set_content_type_from_file(req, filepath);
+
+  char* chunk = rest_context->scratch;
+  ssize_t read_bytes;
+  do {
+    /* Read file in chunks into the scratch buffer */
+    read_bytes = read(fd, chunk, SCRATCH_BUFSIZE);
+    if (read_bytes == -1) {
+      ESP_LOGE(REST_TAG, "Failed to read file : %s", filepath);
+    } else if (read_bytes > 0) {
+      /* Send the buffer contents as HTTP response chunk */
+      if (httpd_resp_send_chunk(req, chunk, read_bytes) != ESP_OK) {
+        close(fd);
+        ESP_LOGE(REST_TAG, "File sending failed!");
+        /* Abort sending file */
+        httpd_resp_sendstr_chunk(req, NULL);
+        /* Respond with 500 Internal Server Error */
+        httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR,
+                            "Failed to send file");
+        return ESP_FAIL;
+      }
+    }
+  } while (read_bytes > 0);
+  /* Close file after sending complete */
+  close(fd);
+  ESP_LOGI(REST_TAG, "File sending complete");
+  /* Respond with an empty chunk to signal HTTP response completion */
+  httpd_resp_send_chunk(req, NULL, 0);
+  return ESP_OK;
+}
+
+/* Simple handler for getting system handler */
+static esp_err_t system_info_get_handler(httpd_req_t* req) {
+  httpd_resp_set_type(req, "application/json");
+  cJSON* root = cJSON_CreateObject();
+  esp_chip_info_t chip_info;
+  esp_chip_info(&chip_info);
+  cJSON_AddStringToObject(root, "version", IDF_VER);
+  cJSON_AddNumberToObject(root, "cores", chip_info.cores);
+  const char* sys_info = cJSON_Print(root);
+  httpd_resp_sendstr(req, sys_info);
+  free((void*)sys_info);
+  cJSON_Delete(root);
+  return ESP_OK;
+}
+
+
+
+httpd_handle_t start_rest_server(const char* base_path) {
+  if (!base_path) {
+    ESP_LOGE(REST_TAG, "Wrong base path");
+    return nullptr;
+  }
+  rest_server_context_t* rest_context = static_cast<rest_server_context_t*>(
+      calloc(1, sizeof(rest_server_context_t)));
+  if (!rest_context) {
+    ESP_LOGE(REST_TAG, "No memory for rest context");
+    return nullptr;
+  }
+  strlcpy(rest_context->base_path, base_path, sizeof(rest_context->base_path));
+
+  httpd_handle_t server = NULL;
+  httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+  config.max_uri_handlers = 10;
+  config.uri_match_fn = httpd_uri_match_wildcard;
+
+  ESP_LOGI(REST_TAG, "Starting HTTP Server");
+  if (!httpd_start(&server, &config) == ESP_OK) {
+    ESP_LOGE(REST_TAG, "Start rest server failed");
+    free(rest_context);
+    return nullptr;
+  }
+
+  /* URI handler for fetching system info */
+  httpd_uri_t system_info_get_uri = {.uri = "/api/v1/system/info",
+                                     .method = HTTP_GET,
+                                     .handler = system_info_get_handler,
+                                     .user_ctx = rest_context};
+  httpd_register_uri_handler(server, &system_info_get_uri);
+
+  // NEXT PART IS LAST, DONT MODIFY
+  /* URI handler for getting web server files */
+  httpd_uri_t common_get_uri = {.uri = "/*",
+                                .method = HTTP_GET,
+                                .handler = rest_common_get_handler,
+                                .user_ctx = rest_context};
+  httpd_register_uri_handler(server, &common_get_uri);
+
+  return server;
+}
+
+// static void initialise_mdns(void) {
+//   mdns_init();
+//   mdns_hostname_set(CONFIG_MDNS_HOST_NAME);
+//   mdns_instance_name_set(MDNS_INSTANCE);
+
+//   mdns_txt_item_t serviceTxtData[] = {{"board", "esp32"}, {"path", "/"}};
+
+//   ESP_ERROR_CHECK(
+//       mdns_service_add("ESP32-WebServer", "_http", "_tcp", 80, serviceTxtData,
+//                        sizeof(serviceTxtData) / sizeof(serviceTxtData[0])));
+// }
+
+esp_err_t init_fs(void) {
+  esp_vfs_spiffs_conf_t conf = {.base_path = CONFIG_WEB_MOUNT_POINT,
+                                .partition_label = NULL,
+                                .max_files = 5,
+                                .format_if_mount_failed = false};
+  esp_err_t ret = esp_vfs_spiffs_register(&conf);
+
+  if (ret != ESP_OK) {
+    if (ret == ESP_FAIL) {
+      HERROR("Failed to mount or format filesystem");
+    } else if (ret == ESP_ERR_NOT_FOUND) {
+      HERROR("Failed to find SPIFFS partition");
+    } else {
+      HERROR("Failed to initialize SPIFFS (%s)", esp_err_to_name(ret));
+    }
+    return ESP_FAIL;
+  }
+
+  size_t total = 0, used = 0;
+  ret = esp_spiffs_info(NULL, &total, &used);
+  if (ret != ESP_OK) {
+    HERROR("Failed to get SPIFFS partition information (%s)",
+             esp_err_to_name(ret));
+  } else {
+    HINFO("Partition size: total: %d, used: %d", total, used);
+  }
+  return ESP_OK;
+}
+
+void rest_initialize() {
+  init_fs();
+  start_rest_server(CONFIG_WEB_MOUNT_POINT);
+
+  // some inits for the rest server
+  // initialise_mdns();
+  // netbiosns_init();
+  // netbiosns_set_name(CONFIG_MDNS_HOST_NAME);
+}
+
+}  // namespace hakkou
---
+++ main/services/wifi.h
@@ -0,0 +1,155 @@
+#pragma once
+
+#include "logger.h"
+
+/* WiFi station Example
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include "esp_event.h"
+#include "esp_log.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
+#include "freertos/task.h"
+#include "nvs_flash.h"
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+#include <string>
+#include <string.h>
+
+/* The examples use WiFi configuration that you can set via project
+   configuration menu If you'd rather not, just change the below entries to
+   strings with the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
+*/
+static const std::string WIFI_SSID = CONFIG_WIFI_SSID;
+static const std::string WIFI_PASS = CONFIG_WIFI_PASSWORD;
+#define MAXIMUM_RETRY CONFIG_WIFI_MAXIMUM_RETRY // TODO: get rid of define
+
+#if CONFIG_WIFI_AUTH_OPEN
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_OPEN
+#elif CONFIG_WIFI_AUTH_WEP
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WEP
+#elif CONFIG_WIFI_AUTH_WPA_PSK
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA_PSK
+#elif CONFIG_WIFI_AUTH_WPA2_PSK
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA2_PSK
+#elif CONFIG_WIFI_AUTH_WPA_WPA2_PSK
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA_WPA2_PSK
+#elif CONFIG_WIFI_AUTH_WPA3_PSK
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA3_PSK
+#elif CONFIG_WIFI_AUTH_WPA2_WPA3_PSK
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA2_WPA3_PSK
+#elif CONFIG_WIFI_AUTH_WAPI_PSK
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WAPI_PSK
+#endif
+
+/* FreeRTOS event group to signal when we are connected*/
+static EventGroupHandle_t s_wifi_event_group;
+
+/* The event group allows multiple bits for each event, but we only care about
+ * two events:
+ * - we are connected to the AP with an IP
+ * - we failed to connect after the maximum amount of retries */
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT BIT1
+
+namespace hakkou {
+
+static const char* WIFI_TAG = "wifi station";
+
+static int s_retry_num = 0;
+
+static void event_handler(void* arg,
+                          esp_event_base_t event_base,
+                          int32_t event_id,
+                          void* event_data) {
+  if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+    esp_wifi_connect();
+  } else if (event_base == WIFI_EVENT &&
+             event_id == WIFI_EVENT_STA_DISCONNECTED) {
+    if (s_retry_num < MAXIMUM_RETRY) {
+      esp_wifi_connect();
+      s_retry_num++;
+      ESP_LOGI(WIFI_TAG, "retry to connect to the AP");
+    } else {
+      xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+    }
+    ESP_LOGI(WIFI_TAG, "connect to the AP fail");
+  } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+    ip_event_got_ip_t* event = (ip_event_got_ip_t*)event_data;
+    ESP_LOGI(WIFI_TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+    s_retry_num = 0;
+    xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+  }
+}
+
+void wifi_init_sta(void) {
+  s_wifi_event_group = xEventGroupCreate();
+
+  ESP_ERROR_CHECK(esp_netif_init());
+
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
+  esp_netif_create_default_wifi_sta();
+
+  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+  ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+  esp_event_handler_instance_t instance_any_id;
+  esp_event_handler_instance_t instance_got_ip;
+  ESP_ERROR_CHECK(esp_event_handler_instance_register(
+      WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL, &instance_any_id));
+  ESP_ERROR_CHECK(esp_event_handler_instance_register(
+      IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL, &instance_got_ip));
+
+  // TODO: until c++20 issupproted, we need to initialize the wificonfig a bit
+  // hackily first, and -mandatorily- zero initialize the wifi config (some
+  // values must be set to zero)
+  wifi_config_t wifi_config = {};
+  // then manually update the relevant entries
+  memcpy(wifi_config.sta.ssid, WIFI_SSID.data(),
+         WIFI_SSID.length());
+  memcpy(wifi_config.sta.password, WIFI_PASS.data(),
+         WIFI_PASS.length());
+  wifi_config.sta.threshold.authmode = ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD;
+
+  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+  ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+  ESP_ERROR_CHECK(esp_wifi_start());
+
+  ESP_LOGI(WIFI_TAG, "wifi_init_sta finished.");
+
+  /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or
+   * connection failed for the maximum number of re-tries (WIFI_FAIL_BIT). The
+   * bits are set by event_handler() (see above) */
+  EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+                                         WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+                                         pdFALSE, pdFALSE, portMAX_DELAY);
+
+  /* xEventGroupWaitBits() returns the bits before the call returned, hence we
+   * can test which event actually happened. */
+  if (bits & WIFI_CONNECTED_BIT) {
+    ESP_LOGI(WIFI_TAG, "connected to ap SSID:%s password:%s",
+             WIFI_SSID.c_str(), WIFI_PASS.c_str());
+  } else if (bits & WIFI_FAIL_BIT) {
+    ESP_LOGI(WIFI_TAG, "Failed to connect to SSID:%s, password:%s",
+             WIFI_SSID.c_str(), WIFI_PASS.c_str());
+  } else {
+    ESP_LOGE(WIFI_TAG, "UNEXPECTED EVENT");
+  }
+}
+
+
+
+bool wifi_initialize() {
+  HINFO("[WIFI] Starting wifi");
+  wifi_init_sta();
+  return true; // success
+}
+
+}  // namespace hakkou
